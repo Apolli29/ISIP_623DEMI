6f4959db1fcb885f76b88e45b631f099c8915947
234922 <234922@KB37-118-C11>
2025-12-01 11:43:34 +0300

    добвила базу 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,60 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+
+        static Random random = new Random();
+        static int carsProcessed = 0;
+        static int successfulRepairs = 0;
+        static int failedRepairs = 0;
+
         static void Main(string[] args)
         {
+            var player = Core.Context.player.FirstOrDefault();
+            if (player == null)
+            {
+                player = new player { cash = 5000 };
+                Core.Context.player.Add(player);
+                Core.Context.SaveChanges();
+                Console.WriteLine("Создан новый игрок!");
+            }
+
+            bool gameRunning = true;
+
+            while (gameRunning)
+            {
+
+                ShowPlayerStatus(player);
+                Console.WriteLine("\n1 - Обслужить следующего клиента");
+                Console.WriteLine("2 - Купить запчасти");
+                Console.WriteLine("3 - Посмотреть склад");
+                Console.WriteLine("4 - Выход");
+
+                var choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        ProcessNextCar(player);
+                        break;
+                    case "2":
+                        ShowStoreMenu(player);
+                        break;
+                    case "3":
+                        ShowInventory(player);
+                        break;
+                    case "4":
+                        gameRunning = false;
+                        break;
+                }
+
+                if (choice != "4")
+                {
+                    Console.WriteLine("\nНажмите любую клавишу для продолжения...");
+                    Console.ReadKey();
+                }
+            }
+
+            Console.WriteLine("Игра завершена!");
         }
-    }
-}
+
+
