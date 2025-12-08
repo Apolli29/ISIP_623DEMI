8033aad2d8482c53d1dc747d83e9678cb9c0e3c5
234922 <234922@KB37-118-C10>
2025-12-08 12:07:20 +0300

    ещё

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -17,7 +17,7 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            var player = Core.Context.player.FirstOrDefault();
+            var player = Core.Context.player.FirstOrDefault(p => p.ID == player.ID);
             if (player == null)
             {
                 player = new player { cash = 5000 };
