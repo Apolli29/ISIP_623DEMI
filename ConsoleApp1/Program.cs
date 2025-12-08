033529270cd60fad3f8185905489a306a1375979
234922 <234922@KB37-118-C10>
2025-12-08 10:54:58 +0300

    доставка

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -292,7 +292,35 @@ namespace ConsoleApp1
                 Console.WriteLine("Недостаточно денег!");
             }
         }
+        private static void ProcessDeliveries(player player)
+        {
+            var orders = Core.Context.OrderParts.Where(o => o.PlayerID == player.id).ToList();
+            foreach (var order in orders)
+            {
+                order.carsUntilDelivery--;
+                if (order.carsUntilDelivery <= 0)
+                {
+                    // Доставляем детали на склад
+                    var inventory = Core.Context.parts_player.FirstOrDefault(i =>
+                        i.idPlayer == player.id && i.idPart == order.PartID);
 
+                    if (inventory == null)
+                    {
+                        inventory = new parts_player
+                        {
+                            idPlayer = player.id,
+                            idPart = order.PartID,
+                            countParts = 0
+                        };
+                        Core.Context.parts_player.Add(inventory);
+                    }
+
+                    inventory.countParts += order.count;
+                    Core.Context.OrderParts.Remove(order);
+                }
+            }
+            Core.Context.SaveChanges();
+        }
     }
 
 
